10e267efe8a0bab2320926130950c627e4b1f00f
Jhon Idrovo <jhonidrovo3@gmail.com>
2021-08-02 08:00:14 -0500

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+import "../styles/global.css";
+import NavBar from "../components/NavBar";
+import { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
+}
